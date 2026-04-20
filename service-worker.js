3471d2389e2321e7f4212674bebbe91a5ec5315c
mcerritog <martin_2505@hotmail.com>
2026-04-19 21:57:15 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('app').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json'
+      ]);
+    })
+  );
+});
